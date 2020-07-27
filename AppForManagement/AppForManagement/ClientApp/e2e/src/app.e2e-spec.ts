5fadc17b138f5c73aeae980afa7aa9c5c9452207
EvgeniyDev <zh.hmelevskoj2013@gmail.com>
2020-07-27 12:34:55 +0300

    e2e testing
    - Implemented 1 e2e test on Protractor, that checks 'Create note' button, if it's enabled or not, on validation

--- AppForManagement/AppForManagement/ClientApp/e2e/src/app.e2e-spec.ts
+++ AppForManagement/AppForManagement/ClientApp/e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { element, by, browser } from 'protractor';
 
 describe('App', () => {
   let page: AppPage;
@@ -7,8 +8,34 @@ describe('App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getMainHeading()).toEqual('Hello, world!');
+  it('Should enable "Create note" button only if input fields are not empty', () => {
+    page.navigateTo('/');
+
+    let createNoteLink = element(by.linkText('Create Note'));
+
+    let dummyName = 'One';
+    let dummySurname = 'Two';
+    let dummyAge = 18;
+
+    let nameInputField = element(by.id('name'));
+    let surnameInputField = element(by.id('surname'));
+    let ageInputField = element(by.id('age'));
+
+    let createNoteButton = element(by.buttonText('Create note'));
+
+    createNoteLink.click();
+
+    nameInputField.click();
+    nameInputField.sendKeys(dummyName);
+    
+    surnameInputField.click();
+    surnameInputField.sendKeys(dummySurname);
+
+    expect(createNoteButton.isEnabled()).toBe(false);
+
+    ageInputField.click();
+    ageInputField.sendKeys(dummyAge);
+
+    expect(createNoteButton.isEnabled()).toBe(true);
   });
 });
--- AppForManagement/AppForManagement/ClientApp/e2e/src/app.po.ts
+++ AppForManagement/AppForManagement/ClientApp/e2e/src/app.po.ts
@@ -1,11 +1,7 @@
-import { browser, by, element } from 'protractor';
+import { browser } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getMainHeading() {
-    return element(by.css('app-root h1')).getText();
+  navigateTo(url: string) {
+    return browser.get(url);
   }
 }
